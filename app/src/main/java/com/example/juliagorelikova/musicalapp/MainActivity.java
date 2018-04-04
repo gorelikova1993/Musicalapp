cd5b3288531c5d375869f68b71cfb276b55e00e7
Yulia Gorelikova <julia.gorelikova@OCRV.RU>
2018-04-04 11:15:32 +0300

    change activity_main

--- app/src/main/java/com/example/juliagorelikova/musicalapp/MainActivity.java
+++ app/src/main/java/com/example/juliagorelikova/musicalapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.juliagorelikova.musicalapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
 import android.view.View;
 import android.widget.TextView;
 
